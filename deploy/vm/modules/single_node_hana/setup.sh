5fb25de4dc7163994cc9f443eadcd884739304d2
Kunho Ko <bedro96@naver.com>
2020-02-13 16:27:21 +0900

    update

--- deploy/vm/modules/single_node_hana/setup.sh
+++ deploy/vm/modules/single_node_hana/setup.sh
@@ -15,18 +15,18 @@ sed -i "s/VAR_DOMAIN_NAME/$DOMAIN_NAME/" ./terraform.tfvars
 sed -i "s/VAR_VM_SIZE/$VM_SIZE/" ./terraform.tfvars  
 sed -i "s/VAR_VM_USERNAME/$VM_USERNAME/" ./terraform.tfvars
 
-SAPCAR_LINUX_URL='https://terraformstate0000.blob.core.windows.net/sap/SAPCAR_1311-80000935.EXE?sp=r&st=2020-02-13T05:59:42Z&se=2022-02-13T13:59:42Z&spr=https&sv=2019-02-02&sr=b&sig=GF8u2mcNvp8E%2BCZXLc'
+SAPCAR_LINUX_URL='https://terraformstate0000.blob.core.windows.net/sap/SAPCAR_1311-80000935.EXE?sp=r&st=2020-02-13T07:17:55Z&se=2025-02-13T15:17:55Z&spr=https&sv=2019-02-02&sr=b&sig=Y4svp7XvSqUeIdlrThrBLkJyrA%2Brkg7YRjDgeXj%2Bcog%3D'
 SAPCAR_LINUX_URL_REGEX="$(echo $SAPCAR_LINUX_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
-SAPCAR_WINDOWS_URL='https://terraformstate0000.blob.core.windows.net/sap/SAPCAR_1311-80000938.EXE?sp=r&st=2020-02-13T06:01:26Z&se=2022-02-13T14:01:26Z&spr=https&sv=2019-02-02&sr=b&sig=ut%2FSNlG1JjFHfoXn62aXL5hnmnVeiWWkvIELQ3vmOpM%3D'
+SAPCAR_WINDOWS_URL='https://terraformstate0000.blob.core.windows.net/sap/SAPCAR_1311-80000938.EXE?sp=r&st=2020-02-13T07:21:16Z&se=2025-02-13T15:21:16Z&spr=https&sv=2019-02-02&sr=b&sig=1JSZ5zUYdl3CqsEFN0uSeD0RXbJ88PyZVQalPUxrtIg%3D'
 SAPCAR_WINDOWS_URL_REGEX="$(echo $SAPCAR_WINDOWS_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 
-HDBSERVER_URL='https://terraformstate0000.blob.core.windows.net/sap/IMDB_SERVER100_122_29-10009569.SAR?sp=r&st=2020-02-13T06:02:40Z&se=2022-02-13T14:02:40Z&spr=https&sv=2019-02-02&sr=b&sig=zc8pL7creucm7cR9qbVp1g%2BRtRl%2FPoVv%2FxBXTADUSUc%3D'
+HDBSERVER_URL='https://terraformstate0000.blob.core.windows.net/sap/IMDB_SERVER100_122_29-10009569.SAR?sp=r&st=2020-02-13T07:22:40Z&se=2025-02-13T15:22:40Z&spr=https&sv=2019-02-02&sr=b&sig=OsR9um%2FaTyCMKYS4OkZZjw6V6ifvgaQbbupWlu4Jrs0%3D'
 HDBSERVER_URL_REGEX="$(echo $HDBSERVER_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 
-SAP_HOST_AGENT_URL='https://terraformstate0000.blob.core.windows.net/sap/SAPHOSTAGENT36_36-20009394.SAR?sp=r&st=2020-02-13T06:04:06Z&se=2022-02-13T14:04:06Z&spr=https&sv=2019-02-02&sr=b&sig=MGAeNa%2BRqgfuKNvTrFJSqh%2FHRm8%2BO7YTX%2FEGhr12sbM%3D'
+SAP_HOST_AGENT_URL='https://terraformstate0000.blob.core.windows.net/sap/SAPHOSTAGENT36_36-20009394.SAR?sp=r&st=2020-02-13T07:24:08Z&se=2025-02-13T15:24:08Z&spr=https&sv=2019-02-02&sr=b&sig=A4aa7q3hBs82cekbvXbZtZBXGjiuUCu4Kby9T9Sw8GI%3D'
 SAP_HOST_AGENT_URL_REGEX="$(echo $SAP_HOST_AGENT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 
-HANA_STUDIO_WINDOWS_URL='https://terraformstate0000.blob.core.windows.net/sap/IMC_STUDIO2_245_0-80000323.SAR?sp=r&st=2020-02-13T06:04:45Z&se=2022-02-13T14:04:45Z&spr=https&sv=2019-02-02&sr=b&sig=4Ydlwy68Uy4GEGMGECq5MrVfZJpKBzIlQmMO10vEu7I%3D'
+HANA_STUDIO_WINDOWS_URL='https://terraformstate0000.blob.core.windows.net/sap/IMC_STUDIO2_245_0-80000323.SAR?sp=r&st=2020-02-13T07:25:09Z&se=2025-02-13T15:25:09Z&spr=https&sv=2019-02-02&sr=b&sig=NiuGX4cmisHokdbC6wmh3xc9Fin%2F1V9yQ%2FwPL%2BPWw4g%3D'
 HANA_STUDIO_WINDOWS_URL_REGEX="$(echo $HANA_STUDIO_WINDOWS_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"
 
 sed -i "s/VAR_SAPCAR_LINUX_URL/$SAPCAR_LINUX_URL_REGEX/" ./terraform.tfvars
